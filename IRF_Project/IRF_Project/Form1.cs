990a2d273cdcfddccd8cec7117b4ee5159853774
Bence Bacsa <bacsabence00@gmail.com>
2020-12-12 13:11:37 +0100

    03

--- IRF_Project/IRF_Project/Form1.cs
+++ IRF_Project/IRF_Project/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using IRF_Project.MnbServiceReference;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,24 @@ namespace IRF_Project
         public Form1()
         {
             InitializeComponent();
+
+            WebServiceCall();
+        }
+
+        private void WebServiceCall()
+        {
+            var mnbService = new MNBArfolyamServiceSoapClient();
+
+            var request1 = new GetExchangeRatesRequestBody()
+            {
+                currencyNames="EUR",
+                startDate="2020-01-01",
+                endDate="2020-06-30"
+            };
+
+            var response1 = mnbService.GetExchangeRates(request1);
+
+            var result1 = response1.GetExchangeRatesResult;
         }
     }
 }
